62e87b66c14044a4303fd700b87aeed3b857e317
ZumuTech <hfuhuang@hotmail.com>
2021-01-14 16:52:07 +0100

    Create ansible_controller_provision_script.sh
    Add script to provision Ansible Controller VM

---
+++ scripts/ansible_controller_provision_script.sh
@@ -0,0 +1,41 @@
+#!/bin/sh
+sudo dnf install python38 python3-libselinux -y
+#sudo dnf install python3-virtualenv -y
+#VENVDIR=venv_p38
+#virtualenv --python=/usr/bin/python3.8 $VENVDIR
+#source $VENVDIR/bin/activate
+sudo pip3 install selinux
+sudo pip3 install -r /vagrant/requirements.txt
+
+# install docker-ce on the ansible controller
+sudo dnf config-manager --add-repo=https://download.docker.com/linux/centos/docker-ce.repo
+sudo dnf install docker-ce --nobest -y
+#sudo pip3 install docker-compose
+sudo usermod -aG docker $USER
+sudo systemctl start docker
+sudo systemctl enable docker
+
+# install nfs server on the ansible controller
+sudo mkdir /srv/nfs/kubedata -p
+#sudo chown nfsnobody: /srv/nfs/kubedata/
+sudo dnf install -y nfs-utils
+sudo systemctl enable nfs-server
+sudo systemctl start nfs-server
+sudo grep -q '^\/srv\/nfs\/kubedata' /etc/exports || (echo '/srv/nfs/kubedata    *(rw,sync,no_subtree_check,no_root_squash,no_all_squash,insecure)' | sudo tee -a /etc/exports )
+sudo exportfs -rav
+
+# install the helm on the ansible controller
+curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3
+chmod 700 get_helm.sh
+./get_helm.sh
+helm repo add stable https://charts.helm.sh/stable
+
+# install the skaffold on the ansible controller
+curl -Lo skaffold https://storage.googleapis.com/skaffold/releases/latest/skaffold-linux-amd64 && \
+sudo install skaffold /usr/local/bin/
+
+# install the kustomize on the ansible controller
+curl -s "https://raw.githubusercontent.com/\
+kubernetes-sigs/kustomize/master/hack/install_kustomize.sh"  | bash
+
+
